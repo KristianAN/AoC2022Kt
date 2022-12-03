f6887a17b1b3839ec440f64d9272da5a64334fde
Kristian Nedrevold <kristian.alvestad.nedrevold@hnikt.no>
2022-12-03 21:21:30 +0100

    day3 improved readability

--- src/Day03.kt
+++ src/Day03.kt
@@ -6,8 +6,8 @@ fun main() {
         s.chunked(s.length / 2).let { it[0].toSet().intersect(it[1].toSet()) }.toCharArray()[0]
     }.sumOf { it.valueOf() }
 
-    fun part2(input: List<String>): Int = input.chunked(3).map {
-        it[0].first { char -> (it[1].contains(char) && it[2].contains(char)) }
+    fun part2(input: List<String>): Int = input.chunked(3).map { rucksacks ->
+        rucksacks[0].first { char -> (rucksacks[1].contains(char) && rucksacks[2].contains(char)) }
     }.sumOf { it.valueOf() }
 
     val input = readInput("day03")
